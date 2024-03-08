72dd782d24ed547e23d337f63b2359c7f11e1daf
Kevin Pudlo <pudlok@gmail.com>
2024-03-08 10:47:38 -0500

    updated wpase array

--- library/php/array-query-wpase-relationship.php
+++ library/php/array-query-wpase-relationship.php
@@ -2,13 +2,10 @@
 
 $list_ids = array();
 if (function_exists('get_cf_related_to')) {
-    $key_ids = get_cf_related_to('wpase_field_slug');
-
-    if ($key_ids) {
-        foreach ($key_ids as $key_id) {
-            $list_ids[] = $key_id->ID;
-        }
-    }
+    $list_ids = get_cf_related_to('wpase_field_slug');
 }
 
-return ['post__in' => $list_ids];
+return [
+    'post__in' => $list_ids,
+    'post_type' => 'any'
+];
